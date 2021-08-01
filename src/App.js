6983554e3b749bf236cd90efc7332ca354972d90
munish710 <munish710@gmail.com>
2021-08-01 23:42:41 +0530

    basic reducer store setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
 // redux stuff
+import { createStore } from "redux";
+import reducer from "./reducer";
+
+//intial store
+const initalStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
+//store
+const store = createStore(reducer, initalStore);
 
 function App() {
   // cart setup
