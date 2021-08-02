649b6a0fb755f3367e609497fe56c9b2ec478ed6
munish710 <munish710@gmail.com>
2021-08-02 23:25:59 +0530

    connected cartItem and used setup remove item functionlaity

--- src/action.js
+++ src/action.js
@@ -1,1 +1,4 @@
 export const CLEAR_CART = "CLEAR_CART";
+export const DECREASE = "DECREASE";
+export const INCREASE = "INCREASE";
+export const REMOVE = "REMOVE";
--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,14 @@
-import { CLEAR_CART } from "./action";
+import { CLEAR_CART, REMOVE } from "./action";
 
 function reducer(state, action) {
   switch (action.type) {
     case CLEAR_CART:
       return { ...state, cart: [] };
+    case REMOVE:
+      return {
+        ...state,
+        cart: state.cart.filter((item) => item.id !== action.payload.id),
+      };
     default:
       return state;
   }
